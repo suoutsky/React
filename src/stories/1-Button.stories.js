85dbd9b3a873950bf325735f5b071d492b4174d8
suxq@yunjiglobal.com <suxq@yunjiglobal.com>
2019-12-09 18:43:39 +0800

    add

--- src/stories/1-Button.stories.js
+++ src/stories/1-Button.stories.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import { action } from '@storybook/addon-actions';
 import { Button } from '@storybook/react/demo';
-
+import { withKnobs } from "@storybook/addon-knobs";
+// import { storiesOf } from '@storybook/react';
 export default {
   title: 'Button',
+  decorators: [withKnobs]
 };
 
 export const text = () => <Button onClick={action('clicked')}>Hello Button</Button>;
